4fcea0003e73641ae803c4f97e4459b0f0ac7b48
SimonHseth <142881727+SimonHseth@users.noreply.github.com>
2024-02-27 23:39:40 +0100

    Yay, det funker

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,30 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import logo192 from "./logo192.png"
+import axios from "axios";
 
-
-export default function SimpleSlider() {
+function Fade() {
   var settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyload: true,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 300,
+
+      }
+    ]
+
   };
+  
+  const [data, setData] = useState();
+  const [name, setName] = useState();
+
+  URL="https://pokeapi.co/api/v2/pokemon/ditto"
+
+  useEffect(() =>{
+    axios.get(URL).then((response) =>{
+      console.log(response.data)
+      setData(response.data)
+      setName(response.data.name)
+    })
+  })
+
   return (
-    <Slider {...settings}>
-      <div>
-        <img src="{`$.{process.env.PUBILC_URL}/public/lawn-mower-2127637_1280.jpg`}" alt="bilde"/>
-        
-      </div>
-      <div>
-        <h1>2</h1>
-      </div>
+    <div class="slider_container">
+      <Slider {...settings}>
+      
+        <div class="Slider_image">
+          <div className="rounded-t-xl">
+            <img src={logo192} alt=""/>
+          </div>
+        </div>
+
+        <div class="Pokemon">
+          <h1>Pokemon</h1>
+          <h2>{ name}</h2>
+          <img src={data?data.sprites.other.dream_world.front_default:"<p>Loading<p>"}/>
+          
+          
+
+        </div>
+      
+
+      
+      
       
       
       
       
     </Slider>
+    </div>
   );
 }
 
+
 console.log(Slider)
+export default Fade;
