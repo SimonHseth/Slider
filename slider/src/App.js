b7fb3623127313415c1cfbeceeae3613ba41e216
SimonHseth <142881727+SimonHseth@users.noreply.github.com>
2024-02-28 14:54:42 +0100

    update

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect ,useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import logo192 from "./logo192.png"
 import axios from "axios";
+import ReactPlayer from "react-player";
+
+
 
 function Fade() {
   var settings = {
@@ -23,19 +26,46 @@ function Fade() {
     ]
 
   };
+
+  const isMobile = window.innerWidth <= 768;
+  const [content, setContent] = useState('');
   
   const [data, setData] = useState();
   const [name, setName] = useState();
+  const [baseExperience, setBaseExpeperience] = useState();
+  
+  
 
-  URL="https://pokeapi.co/api/v2/pokemon/ditto"
+  
 
-  useEffect(() =>{
-    axios.get(URL).then((response) =>{
-      console.log(response.data)
-      setData(response.data)
-      setName(response.data.name)
-    })
-  })
+  if (isMobile) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setContent(`Latitude: ${position.coords.latitude}, Longtitude: ${position.coords.longitude}`);
+    });
+
+    
+  } else {
+    
+      axios.get(URL).then((response) =>{
+        console.log(response.data)
+        setData(response.data)
+        setName(response.data.name)
+        setBaseExpeperience(response.data.baseExperience)
+      })
+
+    
+  }
+  
+  URL="https://pokeapi.co/api/v2/pokemon/charmander"
+
+  if (!isMobile) {
+     
+  
+  }
+
+  
 
   return (
     <div class="slider_container">
@@ -50,19 +80,14 @@ function Fade() {
         <div class="Pokemon">
           <h1>Pokemon</h1>
           <h2>{ name}</h2>
-          <img src={data?data.sprites.other.dream_world.front_default:"<p>Loading<p>"}/>
-          
-          
+          <img src={data?data.sprites.other.dream_world.front_default:"<p>Loading<p>"} alt=""/>
+          <h3>{ baseExperience}</h3>
 
+          <h1>{content}</h1>
         </div>
-      
 
-      
-      
-      
-      
-      
-      
+        
+
     </Slider>
     </div>
   );
