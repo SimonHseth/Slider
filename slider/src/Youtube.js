e56ac94295291b4bf45b40834f1420e61e84ca4f
SimonHseth <Simon.hjelseth@outlook.com>
2024-03-02 21:15:01 +0100

    ds

--- slider/src/Youtube.js
+++ slider/src/Youtube.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player/lazy';
+import ReactPlayer from 'react-player/youtube';
 import axios from 'axios';
 
 const VideoComponent = ({ videoUrl }) => {
