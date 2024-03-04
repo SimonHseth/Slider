23e65fa59a49da9f8b6dfddbe7c987f33ac2401a
SimonHseth <Simon.hjelseth@outlook.com>
2024-03-04 09:03:02 +0100

    ds

---
+++ slider/src/slider.js
@@ -0,0 +1,110 @@
+import React, { useEffect ,useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import horse_450 from "./horse_540.jpg"
+import axios from "axios";
+import VideoComponent from "./Youtube";
+import videoUrl from "./Youtube";
+import ReactPlayer from "./Youtube";
+import metadata from "./Youtube";
+import "./App.css"
+
+
+function Responsive() {
+  var settings = {
+    dots: true,
+    centerMode: true,
+    fade: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyload: true,
+    initialSlide: 2,
+    responsive: [
+      {
+        breakpoint: 10,
+
+      }
+    ]
+
+  };
+
+  const isMobile = window.innerWidth <= 768;
+  const [content, setContent] = useState('');
+  
+  const [data, setData] = useState();
+  const [name, setName] = useState();
+  const [baseExperience, setBaseExpeperience] = useState();
+  
+  
+
+  
+
+  if (isMobile) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setContent(`Latitude: ${position.coords.latitude}, Longtitude: ${position.coords.longitude}`);
+    });
+
+    
+  } else {
+    
+      axios.get(URL).then((response) =>{
+        console.log(response.data)
+        setData(response.data)
+        setName(response.data.name)
+      })
+
+      .catch((error) => {
+        console.error('Feil under Axios-forespørsel:', error);
+      });
+    
+      
+
+    
+  }
+  
+  URL="https://pokeapi.co/api/v2/pokemon/machamp"
+
+  
+
+  return (
+    
+      <Slider {...settings}>
+        
+          <div >
+            <img src={horse_450} alt="bildet er tatt av Timur Romanov, Unsplash"/>
+            <h2>bildet er tatt av Timur Romanov, Unsplash</h2>
+          </div>
+
+        <div >
+          
+          <img src={data?data.sprites.other.dream_world.front_default:"<p>Loading<p>"} alt=""/>
+          <h2>{ name}</h2>
+          <h1>{content}</h1>
+        </div>
+
+        <div className="video-wrapper">
+        <VideoComponent videoUrl="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+        <ReactPlayer url={videoUrl} playing={true} controls={true} />
+        <h2>{metadata.title}</h2>
+        <p>{metadata.description}</p>
+        </div>
+
+
+        
+      
+
+        
+
+    </Slider>
+    
+  );
+}
+
+
+console.log(Slider)
+export default Responsive;
